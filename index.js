84babb4701469effc9738d377454c4933c3ee90a
Sanil-Surve <sanilsurve42@gmail.com>
2025-02-09 20:38:58 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // Change the port if needed
+const PORT = 3001; // Change the port if needed
 
 // Define a simple route
 app.get("/", (req, res) => {
